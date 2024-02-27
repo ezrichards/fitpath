41db124b5c3303111a133e58ad58acf64e5c0ea9
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2024-02-27 11:40:49 -0700

    Flesh out components and add mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '@mantine/core/styles.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Card, Image, Text, Badge, Button, Group } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 
-  return (
-    <>
+export default function App() {
+
+  function Demo() {
+    return (
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
+        <Card.Section>
+          <Image
+            src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+            height={160}
+            alt="Norway"
+          />
+        </Card.Section>
+  
+        <Group justify="space-between" mt="md" mb="xs">
+          <Text fw={500}>Norway Fjord Adventures</Text>
+          <Badge color="pink">On Sale</Badge>
+        </Group>
+  
+        <Text size="sm" c="dimmed">
+          With Fjord Tours you can explore more of the magical fjord landscapes with tours and
+          activities on and around the fjords of Norway
+        </Text>
+  
+        <Button color="blue" fullWidth mt="md" radius="md">
+          Book classic tour now
+        </Button>
+      </Card>
+    );
+  }
+  
+  return <MantineProvider>
+    <Demo/>
+    {/* <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -28,8 +53,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
+    </> */}
+  </MantineProvider>;
 }
 
-export default App
