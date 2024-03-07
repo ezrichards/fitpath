e8169324245e353db646ba2e0f7f2ae4308fcbb2
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2024-03-07 12:05:49 -0700

    Add basic header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import './Header.css'
+
+export default function Header() {
+
+    return (
+        <div className="header">
+            <h1>Fitpath</h1>
+
+            <span id="streak">0🔥</span>
+
+            <span id="pathName">No Path</span>
+        </div>
+    );
+
+}
+
