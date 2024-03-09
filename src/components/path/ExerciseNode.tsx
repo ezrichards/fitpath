f083ad0e4ced5f286fdc3f68a184575ad1461459
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2024-03-09 12:36:16 -0700

    Fix TS complaining

--- src/components/path/ExerciseNode.tsx
+++ src/components/path/ExerciseNode.tsx
@@ -1,10 +1,13 @@
 import { FaStar } from "react-icons/fa";
 import './Path.css'
+import Exercise from "../../types/Exercise";
 
-export default function ExerciseNode() {
+const ExerciseNode = (props: {exercise: Exercise}) => {
     return (
         <div className="node">
             <FaStar size={50} fill='white' />
         </div>
     )
 }
+
+export default ExerciseNode
---
+++ src/types/Exercise.tsx
@@ -0,0 +1,5 @@
+export default interface Exercise {
+    id: Number,
+    name: String,
+    completed: Boolean
+}
